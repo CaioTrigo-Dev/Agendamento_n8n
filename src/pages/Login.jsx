046c0adb510904659e116ab30927f98fdaf29154
Caio Cesar <caiorj95@gmail.com>
2026-03-13 21:32:21 -0300

    Style: Alterado Cor do botão Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -80,7 +80,7 @@ export function Login() {
       <CardFooter className="flex-col gap-2">
         <Button 
         type="submit" 
-        className="w-full"
+        className="w-full bg-blue-600 hover:bg-blue-400"
         onClick={(e)=> handleLogin(email, password, e)}
         >
           Login
